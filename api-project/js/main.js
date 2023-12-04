7013f26ef71f6c57a44d2d7c09d166a57f06b81d
angelicaw133 <angelicawo426@gmail.com>
2023-12-04 10:15:31 -0500

    Update main.js

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,22 +1,31 @@
 import '../css/style.css'
 
-const URL = `https://api.quotable.io/random`;
-async function getData(URL){
-    try {
-        const response = await fetch(URL);
-        if(response.status !=200){
-            throw new Error(response.statusText);
-        }
-        const data = await response.json();
-        document.querySelector("h1").textContent = data.content;
-        console.log(data.content);
-        console.log(data.tags)
-    } catch (error) {
-        console.log(error, "uhoh")
-        document.querySelector("h1").textContent = "awnah";
-    }
-}
-getData(URL);
+// const URL = `https://api.quotable.io/random`;
+// async function getData(URL){
+//     try {
+//         const response = await fetch(URL);
+//         if(response.status !=200){
+//             throw new Error(response.statusText);
+//         }
+//         const data = await response.json();
+//         document.querySelector("h1").textContent = data.content;
+//         console.log(data.content);
+//         console.log(data.tags)
+//     } catch (error) {
+//         console.log(error, "please try again later")
+//         document.querySelector("h1").textContent = "please try again later";
+//     }
+// }
+// getData(URL);
 
-//display cards with songs, click on a card, and a new page with cards with quotes that corresponds with song 
-// ie. 
+const fetch = require('node-fetch');
+
+(async () => {
+  const response = await fetch('https://trefle.io/api/v1/plants?token=BhEkOBlZIXwRP0RrgAiBCFno8pb2jhCtWNCk4dIye9I');
+  if(response.status !=200){
+        throw new Error(response.statusText)
+  }
+  const json = await response.json();
+  console.log(json);
+})();
+console.log('https://trefle.io/api/v1/plants?token=BhEkOBlZIXwRP0RrgAiBCFno8pb2jhCtWNCk4dIye9I')
