b2bb7db3b066305f4f0e585fb0b2d4f70ae11ae0
angelicaw133 <angelicawo426@gmail.com>
2023-12-08 10:15:07 -0500

    12/08

---
+++ api-project/js/dom.js
@@ -0,0 +1,10 @@
+const DOMSelectors = {
+    container: document.querySelector(".container"),
+    btn_container: document.querySelector(".btn_container"),
+    cards: document.querySelectorAll(".card"), 
+    card_container: document.querySelector(".card_container"),
+    h2: document.querySelectorAll("h2"), 
+    image: document.querySelectorAll("img"),
+}
+
+export {DOMSelectors}
