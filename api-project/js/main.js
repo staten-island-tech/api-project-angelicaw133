c7f41326b19a7b67ebf508de93c4fd25a5147dcb
angelicaw133 <angelicawo426@gmail.com>
2023-12-09 22:29:05 -0500

    12/09

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -13,8 +13,6 @@ function make_cards(arr) {
           </div>
         </div>`
   )})};
-
-
 const URL = `https://countriesnow.space/api/v0.1/countries`;
 async function getData(URL) {
   try {
@@ -23,30 +21,11 @@ async function getData(URL) {
       throw new Error(response.statusText);
     }
     const data = await response.json();
-
     make_cards(data.data);
-    
-
-    // let data_arr = data.data;
-    // data_arr.forEach((object)=> console.log(object.country))
   } 
-    
     catch (error) {
     console.log(error, "please try again later");
     document.querySelector("h1").textContent = "please try again later";
   }
 }
 getData(URL);
-
-// const fetch = require('node-fetch');
-
-// (async () => {
-//   const response = await fetch('https://trefle.io/api/v1/plants?token=BhEkOBlZIXwRP0RrgAiBCFno8pb2jhCtWNCk4dIye9I');
-//   if(response.status !=200){
-//         throw new Error(response.statusText)
-//   }
-//   const json = await response.json();
-//   console.log(json);
-//   console.log(data.author);
-// })();
-// console.log('https://trefle.io/api/v1/plants?token=BhEkOBlZIXwRP0RrgAiBCFno8pb2jhCtWNCk4dIye9I')
